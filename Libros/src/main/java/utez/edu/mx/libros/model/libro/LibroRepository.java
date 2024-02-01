76fd571c001f804459d880193b1ca0489f92f919
BenjaminDiazP <20223tn048@utez.edu.mx>
2024-02-01 07:27:47 -0600

    Me di cuenta en la revision del dia jueves del examen que me falto una etiqueta la cual era @Repository

--- Libros/src/main/java/utez/edu/mx/libros/model/libro/LibroRepository.java
+++ Libros/src/main/java/utez/edu/mx/libros/model/libro/LibroRepository.java
@@ -1,9 +1,11 @@
 package utez.edu.mx.libros.model.libro;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository // me falto poner la etiqueta repositorio
 public interface LibroRepository extends JpaRepository<Libro,Long > {
 
     Optional<Libro> findById (Long id);
